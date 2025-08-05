d1aeb8ab65e7207f44ffa8cbe08308c7c9d1757e
Pallerla Akash <pallerla.akash7013@gmail.com>
2025-08-05 16:49:26 +0530

    Add files via upload

---
+++ Insert_Sql_Task2.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO customer(CustomerID, Name, Email, Address)
+VALUES(1, 'Arun', 'Arun@example.com', 'Address1');
+INSERT INTO customer(CustomerID, Name, Email, Address)
+VALUES(2, 'Varun', NULL, 'Address2');
+INSERT INTO customer(CustomerID, Name, Email, Address)
+VALUES(3, 'Tarun', 'Tarun@example.com', NULL)
